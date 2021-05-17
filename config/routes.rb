958b4599f1336c077cb66b66708d3257f490f052
Christian Boehme <c.b.90@gmx.de>
2021-05-17 12:35:00 +0200

    lets save

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  GET "ask", to: "questions_controller#ask"
+
 end
